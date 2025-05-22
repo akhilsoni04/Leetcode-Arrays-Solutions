ae9da8ed873585ff993bce51ba84cca2ff384336
Akhil Soni <akhilsoni2611@gmail.com>
2025-05-22 11:55:55 +0530

    problem 3

---
+++ lengthOfLongestSubstring.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int left = 0;
+        int maxLength = 0;
+
+        HashSet<Character> set = new HashSet<>();
+        for(int i=0; i<n; i++){
+            while(set.contains(s.charAt(i))){
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(i));
+            maxLength = Math.max(maxLength, i-left+1);
+        }
+        return maxLength;
+    }
+}
+// Time Complexity : O(n)
+// Space Complexity : O(min(n, m))
