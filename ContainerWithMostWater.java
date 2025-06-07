6714efa907dd07cb940c3e05f0f7f823b36fe524
Akhil Soni <akhilsoni2611@gmail.com>
2025-06-07 07:32:31 +0530

    Leetcode Problem 11

---
+++ ContainerWithMostWater.java
@@ -0,0 +1,15 @@
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int maxArea = 0;
+        
+        for(int i=0; i<height.length-1; i++){
+            int currentArea = Math.min(height[left], height[right]) * (right-left);
+            maxArea =  Math.max(currentArea, maxArea);
+
+            if(height[left] < height[right]) left ++;
+            else right--;
+        }
+        return maxArea;
+    }
+}
